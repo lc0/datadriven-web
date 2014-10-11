06c5d67fa6307c6f49f376ba12dc66f40385c13d
Sergii Khomenko <sergii.khomenko@stylight.com>
2014-10-11 04:14:54 +0200

    added facebook oauth2

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,55 @@
 // Declare app level module which depends on filters, and services
-angular.module('datadriven', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date'])
+angular.module('datadriven', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date', 'ngFacebook'])
+  .config( function( $facebookProvider ) {
+      $facebookProvider.setAppId('300970376756350');
+    })
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/home/home.html', 
+        templateUrl: 'views/home/home.html',
         controller: 'HomeController'})
+
       .otherwise({redirectTo: '/'});
-  }]);
+  }])
+
+  .run( function( $rootScope ) {
+      // Load the facebook SDK asynchronously
+      (function(){
+         // If we've already installed the SDK, we're done
+         if (document.getElementById('facebook-jssdk')) {return;}
+
+         // Get the first script element, which we'll use to find the parent node
+         var firstScriptElement = document.getElementsByTagName('script')[0];
+
+         // Create a new script element and set its id
+         var facebookJS = document.createElement('script');
+         facebookJS.id = 'facebook-jssdk';
+
+         // Set the new script's source to the source of the Facebook JS SDK
+         facebookJS.src = '//connect.facebook.net/en_US/all.js';
+
+         // Insert the Facebook JS SDK into the DOM
+         firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
+       }());
+    });
+
+var DemoCtrl = function ($scope, $facebook) {
+  $scope.isLoggedIn = false;
+  $scope.login = function() {
+    $facebook.login().then(function() {
+      refresh();
+    });
+  }
+  function refresh() {
+    $facebook.api("/me").then(
+      function(response) {
+        $scope.welcomeMsg = "Welcome " + response.name;
+        $scope.isLoggedIn = true;
+      },
+      function(err) {
+        $scope.welcomeMsg = "Please log in";
+      });
+  }
+
+  refresh();
+};
